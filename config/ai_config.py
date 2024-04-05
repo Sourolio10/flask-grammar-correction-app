7c797fd75ff36a18c833169049254d09c5df809a
Souradeep Ghosh <jeetsouradeep@gmail.com>
2024-04-05 17:45:20 -0400

    Update ai_config.py

--- config/ai_config.py
+++ config/ai_config.py
@@ -1,5 +1,5 @@
 import os
 
-API_TOKEN = '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'
+API_TOKEN = ''
 # https://api-inference.huggingface.co/models/vennify/t5-base-grammar-correction
 API_URL = 'https://api-inference.huggingface.co/models/vennify/t5-base-grammar-correction'
