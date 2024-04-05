4fc7496c3f16d5a5d3aa857eea0e2f7cf7f37afb
Souradeep Ghosh <jeetsouradeep@gmail.com>
2024-04-05 17:43:34 -0400

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,11 +1,11 @@
 import os
 from sqlalchemy import create_engine
 from urllib.parse import quote 
-DB_HOST = 'localhost'
-DB_PORT = 3306
-DB_USERNAME = 'root'
-DB_PASSWORD = 'Sourolo@10'
-DB_NAME = 'grammar'
+DB_HOST = ''
+DB_PORT = '
+DB_USERNAME = ''
+DB_PASSWORD = ''
+DB_NAME = ''
 #mysql://username:password@localhost/db_name
 CONNECTION_STRING = 'mysql://{username}:%s@{host}:{port}/{db_name}'.format(username=DB_USERNAME,host=DB_HOST,port=DB_PORT,db_name=DB_NAME)
 print(CONNECTION_STRING)
