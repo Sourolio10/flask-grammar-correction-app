6901641a871670b51e9368bc1a4a9027e36101a6
Souradeep Ghosh <jeetsouradeep@gmail.com>
2024-04-05 17:47:18 -0400

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,12 +1,14 @@
 import os
 from sqlalchemy import create_engine
-from urllib.parse import quote 
-DB_HOST = ''
-DB_PORT = '
-DB_USERNAME = ''
-DB_PASSWORD = ''
-DB_NAME = ''
-#mysql://username:password@localhost/db_name
-CONNECTION_STRING = 'mysql://{username}:%s@{host}:{port}/{db_name}'.format(username=DB_USERNAME,host=DB_HOST,port=DB_PORT,db_name=DB_NAME)
+from urllib.parse import quote
+
+# Load environment variables
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_USERNAME = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+CONNECTION_STRING = f'mysql://{DB_USERNAME}:%s@{DB_HOST}:{DB_PORT}/{DB_NAME}' % quote(DB_PASSWORD)
 print(CONNECTION_STRING)
-engine = create_engine(CONNECTION_STRING % quote(DB_PASSWORD))
+engine = create_engine(CONNECTION_STRING)
